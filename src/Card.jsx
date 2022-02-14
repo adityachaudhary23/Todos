c7b4cbf1c1af5a2028edf286383882ec52c26981
Aditya <adityachaudary2242@gmail.com>
2022-02-14 12:39:30 +0530

    data stored in firebase

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,35 +1,97 @@
-// import { FirebaseError } from 'firebase/app';
 import React, { useState } from 'react';
 import TodoItems from './TodoItems.jsx';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6QzKXrIQsjKF-hwXQClAMDpTD0NZDfow",
+    authDomain: "to-do-list-e99e2.firebaseapp.com",
+    projectId: "to-do-list-e99e2",
+    storageBucket: "to-do-list-e99e2.appspot.com",
+    messagingSenderId: "157707173794",
+    appId: "1:157707173794:web:578a20b6880e42dd477cfb"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
 
 function Card() {
 
     const [value, setValue] = useState("");
     const [items, setItems] = useState([]);
 
+    db.collection("Todos")
+           .get()
+             .then((snapshot)=>{
+                 const todoItem=snapshot.docs.map((doc)=>{
+                     const docs=doc.data();
+                     docs['id']=doc.id;
+                     return docs;
+                 })
+                 console.log(todoItem);
+                 setItems(todoItem);
+             })
+
     function eventHandler(event) {
         setValue(event.target.value);
     }
 
     function addItem() {
-
-        // firebase
-        //   .firestore
-        //      .collections('todos')
-        //         .add({value})
-        //           .then((docref)=>
-        //           console.log('Product has been added', docref))
-        //             .catch((error)=>console.log(error))
-
-
-        setItems(prevItems => {
-            return [...prevItems, value];
+        db.collection("Todos").add({
+            notes:value
         })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+        
+
+        db.collection("Todos")
+          .get()
+            .then((snapshot)=>{
+                const todoItem=snapshot.docs.map((doc)=>{
+                    const docs=doc.data();
+                    docs['id']=doc.id;
+                    return docs;
+                })
+                console.log(todoItem);
+                setItems(todoItem);
+            })    
+
         setValue("");
     }
 
     function deleteItem(id) {
-        setItems(prevItems => prevItems.filter((item, index) => index !== id))
+
+        const docRef=db.collection('Todos').doc(id);
+
+        docRef.delete()
+          .then(()=>{
+              console.log("Deleted successfully");
+          })
+           .catch((error)=>{
+               console.log("Error: ",error);
+           })
+
+         
+        db.collection("Todos")
+           .get()
+             .then((snapshot)=>{
+                 const todoItem=snapshot.docs.map((doc)=>{
+                     const docs=doc.data();
+                     docs['id']=doc.id;
+                     return docs;
+                 })
+                 console.log(todoItem);
+                 setItems(todoItem);
+             })
+
     }
 
     return <div className='card'>
@@ -40,11 +102,11 @@ function Card() {
         <button onClick={addItem} >
             <span>Add</span>
         </button>
-        
+
         <div id="listcontent">
             <ul>
                 {items.map((todo, index) =>
-                    (<TodoItems key={index} id={index} text={todo} onChecked={deleteItem} />)
+                    (<TodoItems key={index} id={todo.id} text={todo.notes} onChecked={deleteItem} />)
                 )}
             </ul>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as serviceWorkers from './serviceWorker';
-// import * as firebase from 'firebase';
-// import 'firebase/firestore';
-
-// import { initializeApp } from "firebase/app";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA6QzKXrIQsjKF-hwXQClAMDpTD0NZDfow",
-//     authDomain: "to-do-list-e99e2.firebaseapp.com",
-//     projectId: "to-do-list-e99e2",
-//     storageBucket: "to-do-list-e99e2.appspot.com",
-//     messagingSenderId: "157707173794",
-//     appId: "1:157707173794:web:578a20b6880e42dd477cfb"
-//   };
-
-// //  firebase.initializeApp(firebaseConfig);
-// const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />,document.getElementById('root'));
